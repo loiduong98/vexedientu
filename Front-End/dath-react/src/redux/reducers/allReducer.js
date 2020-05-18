63a0927fc18d0bff3c4d6e219a22f8ed7b53b547
loiduong98 <loiduong0511@gmail.com>
2020-05-18 12:59:04 +0700

    merge code cong

--- Front-End/dath-react/src/redux/reducers/allReducer.js
+++ Front-End/dath-react/src/redux/reducers/allReducer.js
@@ -6,15 +6,9 @@ import khachhangReducer from "./khachhangReducer";
 import dslichchayReducer from "./dslichchayReducer";
 
 const allReducer = combineReducers({
-<<<<<<< HEAD
-    dsbenReducer: dsbenReducer,
-    dstuyenReducer: dstuyenReducer,
-    khachhangReducer: khachhangReducer,
-=======
   dsbenReducer: dsbenReducer,
   dstuyenReducer: dstuyenReducer,
   khachhangReducer: khachhangReducer,
   dslichchayReducer: dslichchayReducer,
->>>>>>> 7a3884c5ce99efc5d3b7944662ecc7cad6687b5f
 });
 export default allReducer;
