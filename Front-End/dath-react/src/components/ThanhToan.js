ae949bf0d65dbaecfd9539faf2c4cb10be6b91af
Cong Tran <congcc97@gmail.com>
2020-05-20 14:40:13 +0700

    update payment

--- Front-End/dath-react/src/components/ThanhToan.js
+++ Front-End/dath-react/src/components/ThanhToan.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
 
 export default class ThanhToan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  step1; // dữ liệu bước chọn tuyến
+  step2; // dữ liệu bước chọn ghế
+  step3; // dữ liệu bước điền thông tin khách hàng
+  componentWillMount() {
+    if (typeof Storage !== "undefined") {
+      // get sessionStorage
+      this.step1 = JSON.parse(sessionStorage.getItem("chonTuyen"));
+      this.step2 = JSON.parse(sessionStorage.getItem("chonghe"));
+      this.step3 = JSON.parse(sessionStorage.getItem("thongtinkhachhang"));
+      this.setState({
+        tuyen: this.step1.inputTuyen,
+        ngaydi: this.step1.ngayDi,
+        ghe: this.step2.seatReserved,
+        tongtien: this.step2.tongtien,
+        hoten: this.step3.inputHoTen,
+        sdt: this.step3.inputPhone,
+        email: this.step3.inputEmail,
+        diachi: this.step3.inputAddress,
+      });
+    } else {
+      alert("Vui lòng quay lại trang chủ");
+    }
+  }
+
   render() {
+    var ngaydi = this.state.ngaydi;
+    var tempdate = "";
+    var inngaydi = tempdate.concat(
+      ngaydi.substr(8, 2),
+      " ",
+      ngaydi.substr(4, 1),
+      " ",
+      ngaydi.substr(5, 2),
+      " ",
+      ngaydi.substr(4, 1),
+      " ",
+      ngaydi.substr(0, 4)
+    );
     return (
       <section className="payment" style={{ marginTop: "100px" }}>
         <div className="md-stepper-horizontal">
@@ -68,23 +109,56 @@ export default class ThanhToan extends Component {
                           <table className="table">
                             <tbody>
                               <tr>
-                                <td className="col-xs-3">Tuyến:</td>
-                                <td />
+                                <td>Tuyến</td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {this.state.tuyen}
+                                  </span>
+                                </td>
                               </tr>
                               <tr>
-                                <td>Ngày đi:</td>
-                                <td>Ghế/giường:</td>
+                                <td>Ngày đi </td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {inngaydi}
+                                  </span>
+                                </td>
                               </tr>
                               <tr>
-                                <td>Họ tên:</td>
-                                <td>Ngày sinh:</td>
+                                <td>Ghế </td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {this.state.ghe.map((item) => {
+                                      return item + ",   ";
+                                    })}
+                                  </span>
+                                </td>
                               </tr>
                               <tr>
-                                <td>Email:</td>
-                                <td>SĐT:</td>
+                                <td>Họ Tên </td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {this.state.hoten}
+                                  </span>
+                                </td>
                               </tr>
                               <tr>
-                                <td>Tổng tiền:</td>
+                                <td>Số điện thoại </td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {this.state.sdt}
+                                  </span>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>Tổng tiền </td>
+                                <td>
+                                  <span style={{ fontWeight: "bold" }}>
+                                    {this.state.tongtien
+                                      .toString()
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                                  </span>
+                                </td>
                               </tr>
                             </tbody>
                           </table>
@@ -92,12 +166,9 @@ export default class ThanhToan extends Component {
                       </div>
                       <div className="form-group">
                         <div className="col-lg-12 col-md-12 col-sm-2 col-xs-12 col-ms-12">
-                          <a
-                            href="./check-3.html"
-                            className="btn btn-primary btn-block"
-                          >
-                            Quay lại
-                          </a>
+                          <button className="btn btn-primary btn-block">
+                            Thanh Toán
+                          </button>
                         </div>
                       </div>
                     </form>
