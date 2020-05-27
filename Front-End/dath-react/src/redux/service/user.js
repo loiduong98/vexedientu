847a92f289f3fa94b020a6d6668073529f3e9d06
DQuangKhai <quangkhai27098@gmail.com>
2020-05-27 14:44:49 +0700

    meger

--- Front-End/dath-react/src/redux/service/user.js
+++ Front-End/dath-react/src/redux/service/user.js
@@ -1,13 +1,13 @@
 import Axios from "axios";
 
 class userService {
-    dangNhap(user){
+    dangNhap(user) {
         console.log(user);
-        
+
         return Axios({
-            method:"POST",
-            url:'http://127.0.0.1:8000/api/login',
-            data:user
+            method: "POST",
+            url: 'http://127.0.0.1:8000/api/login',
+            data: user
         })
     }
 }
