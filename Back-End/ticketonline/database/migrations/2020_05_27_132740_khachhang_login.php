6710b58a1bdea55e9117cd16a901e97d7b141237
Laando <tienlando2704@gmail.com>
2020-05-28 18:53:03 +0700

    add migrate login khachhang

---
+++ Back-End/ticketonline/database/migrations/2020_05_27_132740_khachhang_login.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class KhachhangLogin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('khachhang_login', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->number('phone');
+            $table->string('password');
+            $table->rememberToken();
+            $table->string('urlHinh');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('khachhang_login');
+    }
+}
